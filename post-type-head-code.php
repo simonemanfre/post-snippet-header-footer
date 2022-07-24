a39f47f1dc11aa2624448120dea3fa5adcb91c61
simonemanfre <simone.manfredini@trapella.it>
2022-07-24 02:27:13 +0200

    change readme

--- post-type-head-code.php
+++ post-type-head-code.php
@@ -1,21 +1,37 @@
 <?php 
+/**
+ * Plugin Name: Post Snippet - Insert Header e Footer Code in Page, Post and Post Type
+ * Plugin URI: https://www.wpcode.com/
+ * Version: 1.0.0
+ * Requires at least: 4.6
+ * Requires PHP: 5.5
+ * Tested up to: 6.0
+ * Author: Simone Manfredini
+ * Author URI: https://trapstudio.it/
+ * Description: Insert code snippet and script to header and footer section of any page, post or custom post type.
+ * License: GPLv2 or later
+ *
+ * Text Domain: post-snippet
+ * Domain Path: /languages/
+ *
+ * @package WPCode
+ */
+
 /*
-Plugin Name: Post Type Head Code
-Description: It add a metabox to all page and post type to insert code in head section
-Author: Simone Manfredini
-Author URI: https://trapstudio.it/
-Domain Path: /languages/
-Text Domain: pt-head-code
-Version: 1.0.0
-*/
-/*  This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+	Copyright 2019 Simone Manfredini
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 defined( 'ABSPATH' ) || exit; // Exit if accessed directly
--- readme.txt
+++ readme.txt
@@ -1,11 +1,11 @@
-=== Post Type Head Code ===
+=== Post Snippet - Insert Header e Footer Code in Page, Post and Post Type ===
 Contributors: simonemanfre
-Tags: cleanup, clean up, deactivate, disable, style
+Tags: code, css, php, footer, functions, content, post, page, post type, facebook pixel, footer code, footer scripts, footers, google analytics, head, header, header code, header scripts, headers, insert, insert code, insert scripts, js, meta, meta tags, scripts, html, javascript, multisite, code snippets
 Requires at least: 4.6
 Tested up to: 6.0
-Stable tag: 1.0.1
-Requires PHP: 5.6
+Stable tag: 1.0.0
+Requires PHP: 5.5
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-It add a metabox to all page and post type to insert code in head section
+Insert code snippet and script to header and footer section of any page, post or custom post type
